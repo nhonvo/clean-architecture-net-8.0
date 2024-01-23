2961883297a2e244d771525c8ff5b52fea366553
nhonvo <vothuongtruongnhon2002@gmail.com>
2024-01-23 11:05:11 +0700

    add unit test project

---
+++ test/CleanArchiteture.Unittest/Application/BookServiceTest.cs
@@ -0,0 +1,60 @@
+using System.Linq.Expressions;
+using AutoMapper;
+using Clean.Architecture.Application;
+using Clean.Architecture.Application.Services;
+using Clean.Architecture.Domain.Entities;
+using Clean.Architecture.Infrastructure.Interface;
+using Moq;
+
+namespace CleanArchitecture.Unittest.Application;
+
+public class BookServiceTest
+{
+    [Fact]
+    public async Task Get_Returns_PaginationOfBooks()
+    {
+        // Arrange
+        var unitOfWorkMock = new Mock<IUnitOfWork>();
+        var bookRepositoryMock = new Mock<IBookRepository>();
+        unitOfWorkMock.Setup(uow => uow.BookRepository).Returns(bookRepositoryMock.Object);
+        var mapperMock = new Mock<IMapper>();
+
+        var bookService = new BookService(unitOfWorkMock.Object, mapperMock.Object);
+
+        var pageIndex = 1;
+        var pageSize = 10;
+
+        // Act
+        var result = await bookService.Get(pageIndex, pageSize);
+
+        // Assert
+        Assert.Null(result);
+        // Assert.IsType<Pagination<Book>>(result);
+    }
+
+    [Fact]
+    public async Task Get_ById_Returns_Book()
+    {
+        // Arrange
+        var unitOfWorkMock = new Mock<IUnitOfWork>();
+        var bookRepositoryMock = new Mock<IBookRepository>();
+        unitOfWorkMock.Setup(uow => uow.BookRepository).Returns(bookRepositoryMock.Object);
+        var mapperMock = new Mock<IMapper>();
+
+        var bookService = new BookService(unitOfWorkMock.Object, mapperMock.Object);
+
+        var bookId = 1;
+        var expectedBook = new Book { Id = bookId, Title = "Test Book" };
+        bookRepositoryMock.Setup(repo => repo.FirstOrDefaultAsync(It.IsAny<Expression<Func<Book, bool>>>()))
+            .ReturnsAsync(expectedBook);
+
+        // Act
+        var result = await bookService.Get(bookId);
+
+        // Assert
+        Assert.NotNull(result);
+        Assert.IsType<Book>(result);
+        Assert.Equal(expectedBook.Id, result.Id);
+        Assert.Equal(expectedBook.Title, result.Title);
+    }
+}
