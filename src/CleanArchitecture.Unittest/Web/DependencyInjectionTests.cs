3e4ea2a579cc83e5ddc595e275613fdc445d29b7
nhonvo <vothuongtruongnhon2002@gmail.com>
2024-07-17 10:58:19 +0700

    fix unit test

--- src/CleanArchitecture.Unittest/Web/DependencyInjectionTests.cs
+++ src/CleanArchitecture.Unittest/Web/DependencyInjectionTests.cs
@@ -13,7 +13,20 @@ namespace CleanArchitecture.Unittest.Web;
 public class DependencyInjectionTests
 {
     private readonly ServiceProvider _serviceProvider;
-    private readonly AppSettings _appSettings = new();
+    private readonly AppSettings _appSettings = new()
+    {
+        ApplicationDetail = new ApplicationDetail
+        {
+            ApplicationName = "app",
+            ContactWebsite = "http://dummy.html",
+            Description = "description",
+            LicenseDetail = "dummy"
+        },
+        ConnectionStrings = new ConnectionStrings
+        {
+            DefaultConnection = "dummy"
+        }
+    };
 
     public DependencyInjectionTests()
     {
@@ -32,7 +45,6 @@ public class DependencyInjectionTests
         var exceptionMiddlewareResolved = _serviceProvider.GetRequiredService<GlobalExceptionMiddleware>();
         var performanceMiddleware = _serviceProvider.GetRequiredService<PerformanceMiddleware>();
         var stopwatchResolved = _serviceProvider.GetRequiredService<Stopwatch>();
-
 
         Assert.Equal(typeof(CurrentTime), currentTimeServiceResolved.GetType());
         Assert.Equal(typeof(GlobalExceptionMiddleware), exceptionMiddlewareResolved.GetType());
