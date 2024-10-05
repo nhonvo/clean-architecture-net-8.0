027ecc117a53d9ddf0326c3a06f6f850ccc4016b
nhonvo <vothuongtruongnhon2002@gmail.com>
2024-10-06 04:35:35 +0700

    remove license variable

--- src/CleanArchitecture/Application/Common/AppSettings.cs
+++ src/CleanArchitecture/Application/Common/AppSettings.cs
@@ -18,7 +18,6 @@ public class ApplicationDetail
     public string ApplicationName { get; set; }
     public string Description { get; set; }
     public string ContactWebsite { get; set; }
-    public string LicenseDetail { get; set; }
 }
 
 public class ConnectionStrings
